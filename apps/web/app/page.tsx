83a8652b77e7fe7d2e66fd388e276f4f87d52afe
Saikat Mitra <saikatmitra91@gmail.com>
2024-03-14 16:02:35 +0530

    feat: add base setup

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Page(): JSX.Element {
+  return (
+    <main> Hello World!</main>
+  );
+}
---
+++ packages/cli/src/runs/config/index.ts
@@ -0,0 +1,18 @@
+import { RunsConfig } from "../../types";
+import { config as OpenAIRunsConfig } from "./defaults";
+
+export enum DefaultRunsConfigType {
+    DEFAULT = "default"
+}
+
+const map = new Map<DefaultRunsConfigType, RunsConfig>([
+    [DefaultRunsConfigType.DEFAULT, OpenAIRunsConfig]
+]);
+
+export const getDefaultRunsConfig = (name: DefaultRunsConfigType): RunsConfig => {
+    if (map.has(name)) {
+        return map.get(name)!;
+    } else {
+        throw new Error(`No default runs config named "${name}".`);
+    }
+};

b4683c604ea9db43dd4ba0defd727dedf12444df
Arjun Attam <arjunattam@gmail.com>
2024-03-20 20:22:12 +0530

    add py to build step

--- packages/evals/src/provider/deterministic/script.ts
+++ packages/evals/src/provider/deterministic/script.ts
@@ -2,37 +2,12 @@ import { Scorer } from "../../interface/scorer";
 import { inputsForReplacements } from "../../utils";
 import { PythonShell } from "python-shell";
 import path from "path";
-import { promises as fs } from "fs";
-
-const pythonWrapper = `
-import json
-import sys
-import importlib
-
-
-sys.path.append(sys.argv[1])
-user_module = importlib.import_module(sys.argv[2])
-
-result = user_module.evaluate(sys.argv[3], json.loads(sys.argv[4]))
-print(json.dumps(result))
-`;
 
 export const name = "py-script";
 
 const scriptTimeout = 2000;
-const wrapperScriptDirectory = path.join(__dirname, "py");
+const wrapperScriptDirectory = path.join(__dirname, "..", "..", "python");
 const wrapperScriptFile = "wrapper.py";
-
-async function writeIfWrapperFileIsMissing() {
-  const wrapperPath = path.join(wrapperScriptDirectory, wrapperScriptFile);
-  try {
-    await fs.readFile(wrapperPath);
-  } catch (err) {
-    // File doesn't exist, so write it
-    await fs.mkdir(wrapperScriptDirectory, { recursive: true });
-    await fs.writeFile(wrapperPath, pythonWrapper);
-  }
-}
 
 export const scoreWithPythonScript: Scorer = async (
   sample,
@@ -47,7 +22,6 @@ export const scoreWithPythonScript: Scorer = async (
     };
   }
 
-  await writeIfWrapperFileIsMissing();
   let inputsAsMap: any = inputsForReplacements(sample.inputs);
   let basePath = path.dirname(userScriptPath);
   let moduleName = path.basename(userScriptPath).replace(".py", "");

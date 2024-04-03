3ff96c72fd1062a06c7ff6ad7e9831afb9f88cb7
Arjun Attam <arjunattam@gmail.com>
2024-04-03 12:54:42 +0530

    review

--- packages/cli/src/reporters/ci.ts
+++ packages/cli/src/reporters/ci.ts
@@ -6,30 +6,32 @@ async function prepareOutputForPRComment(
   completion: RunCompletion[],
   dataset: Dataset,
 ) {
-  if (process.env.GITHUB_OUTPUT) {
-    const failedOutputs = failedOutputsSummary(completion);
-    let failedSummary = "";
-
-    if (failedOutputs) {
-      const { code, message } = failedOutputs;
-      failedSummary = `**Error**: Some outputs were not generated successfully\n${code}: ${message}`;
-    }
-
-    const datasetLength = `Total dataset samples: ${dataset.samples?.length || 0}`;
-    // Multiline output
-    // https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#multiline-strings
-    await fs.appendFile(
-      process.env.GITHUB_OUTPUT,
-      `result<<EOF\n${markdownSummary(completion)}\n${datasetLength}\n\n${failedSummary}\nEOF`,
-    );
+  if (!process.env.GITHUB_OUTPUT) {
+    return;
   }
+
+  const failedOutputs = failedOutputsSummary(completion);
+  let failedSummary = "";
+
+  if (failedOutputs) {
+    const { code, message } = failedOutputs;
+    failedSummary = `**Error**: Some outputs were not generated successfully\n${code}: ${message}`;
+  }
+
+  const datasetLength = `Total dataset samples: ${dataset.samples?.length || 0}`;
+  // Multiline output
+  // https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#multiline-strings
+  await fs.appendFile(
+    process.env.GITHUB_OUTPUT,
+    `result<<EOF\n${markdownSummary(completion)}\n${datasetLength}\n\n${failedSummary}\nEOF`,
+  );
 }
 
 export async function reportOnCI(
   completion: RunCompletion[],
   dataset: Dataset,
 ) {
-  if (process.env.GITHUB_ACTIONS === "true") {
+  if (process.env.GITHUB_ACTIONS === "true" && process.env.GITHUB_OUTPUT) {
     await prepareOutputForPRComment(completion, dataset);
   }
 }

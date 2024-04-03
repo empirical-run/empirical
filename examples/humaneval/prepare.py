caca7a25964fb46a921f01a2b2e60eb510746c40
Arjun Attam <arjunattam@gmail.com>
2024-04-03 20:48:43 +0530

    chore: clean up humaneval example

--- examples/humaneval/prepare.py
+++ examples/humaneval/prepare.py
@@ -9,12 +9,11 @@ def download_and_extract():
         "https://github.com/openai/human-eval/raw/master/data/HumanEval.jsonl.gz"
     )
     destination_path = "HumanEval.jsonl.gz"
-    final_path = os.path.join(".empiricalrun", "HumanEval.jsonl")
     urlretrieve(dataset_url, destination_path)
     os.makedirs(".empiricalrun", exist_ok=True)
 
     with gzip.open(destination_path, "rb") as f_in:
-        with open(final_path, "wb") as f_out:
+        with open("HumanEval.jsonl", "wb") as f_out:
             shutil.copyfileobj(f_in, f_out)
 
     os.remove(destination_path)

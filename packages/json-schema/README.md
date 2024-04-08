44f0639c34d5e3bd890ed8965227812582ad4efd
Arjun Attam <arjunattam@gmail.com>
2024-04-09 01:27:39 +0530

    chore: generate json schema programmatically (#97)
    Co-authored-by: Saikat Mitra <saikatmitra91@gmail.com>

---
+++ packages/json-schema/README.md
@@ -0,0 +1,4 @@
+# json-schema
+
+maintains schema store format json for `.empiricalrc.json`. visit https://www.schemastore.org/json/ to understand more on schema store format.
+
---
+++ packages/json-schema/src/index.ts
@@ -0,0 +1,21 @@
+import { createGenerator, Config } from "ts-json-schema-generator";
+import path from "path";
+import fs from "fs";
+import packageJson from "./../../cli/package.json";
+
+const tsconfigPath = path.join(__dirname, "./../../cli/tsconfig.json");
+const typesPath = path.join(__dirname, "./../../cli/src/types/index.ts");
+
+const config: Config = {
+  path: typesPath,
+  tsconfig: tsconfigPath,
+  type: "RunsConfig",
+};
+const schema = createGenerator(config).createSchema(config.type);
+const schemaString = JSON.stringify(schema, null, 2);
+const outputPath = path.join(__dirname, "./../dist");
+fs.mkdirSync(path.join(__dirname, "./../dist"), { recursive: true });
+// create a version file
+fs.writeFileSync(`${outputPath}/v${packageJson.version}.json`, schemaString);
+// create latest version file
+fs.writeFileSync(`${outputPath}/latest.json`, schemaString);

9489d4d386df2fa7f793dc74406bf0ca18859098
Saikat Mitra <saikatmitra91@gmail.com>
2024-04-11 14:50:00 +0530

    feat: add duckdb filtering on the browser

--- apps/web/hooks/useRunResultTableView.ts
+++ apps/web/hooks/useRunResultTableView.ts
@@ -1,5 +1,56 @@
 import { useCallback, useMemo, useState } from "react";
 import { RunResult } from "../types";
+import * as duckdb from "@duckdb/duckdb-wasm";
+//@ts-ignore
+import duckdb_wasm from "@duckdb/duckdb-wasm/dist/duckdb-mvp.wasm";
+//@ts-ignore
+import duckdb_wasm_next from "@duckdb/duckdb-wasm/dist/duckdb-eh.wasm";
+//@ts-ignore
+
+if (typeof window !== "undefined") {
+  window.initiateDB = async function () {
+    const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
+      mvp: {
+        mainModule: duckdb_wasm,
+        mainWorker: new URL(
+          "@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js",
+          import.meta.url,
+        ).toString(),
+      },
+      eh: {
+        mainModule: duckdb_wasm_next,
+        mainWorker: new URL(
+          "@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js",
+          import.meta.url,
+        ).toString(),
+      },
+    };
+    // Select a bundle based on browser checks
+    const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
+    const worker = new Worker(bundle.mainWorker!);
+    const logger = new duckdb.ConsoleLogger();
+    const db = new duckdb.AsyncDuckDB(logger, worker);
+    await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+    const conn = await db.connect();
+    const streamResponse = await fetch(
+      `https://assets-test.empirical.run/runs%2F102%2Foutput.json`,
+    );
+    const data = await streamResponse.json();
+
+    // await db.registerFileBuffer(
+    //   "file.json",
+    //   new Uint8Array(await streamResponse.arrayBuffer()),
+    // );
+    // await conn.insertJSONFromPath("file.json", {
+    //   name: "file",
+    // });
+    // ... or column-major format
+    await db.registerFileText("columns.json", JSON.stringify(data));
+    // ... with typed insert options
+    await conn.insertJSONFromPath("columns.json", { name: "columns" });
+    return conn;
+  };
+}
 
 export type RunResultTableHeader = {
   title: string;

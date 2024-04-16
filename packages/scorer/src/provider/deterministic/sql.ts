b065dad9724948fb8c0d778f0ffb3ddb12eb49cd
Arjun Attam <arjunattam@gmail.com>
2024-04-16 18:46:16 +0530

    chore: nicer message string for sql-syntax scorer (#142)

--- packages/scorer/src/provider/deterministic/sql.ts
+++ packages/scorer/src/provider/deterministic/sql.ts
@@ -28,7 +28,7 @@ export const checkSqlSyntax: ScoringFn = async ({ output }) => {
     {
       score: isSQLQuery ? 1 : 0,
       name: syntaxName,
-      message: isSQLQuery ? "" : errorMsg,
+      message: isSQLQuery ? "Output is valid SQL" : errorMsg,
     },
   ];
 };

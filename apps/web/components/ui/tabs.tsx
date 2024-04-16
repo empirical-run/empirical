add3b4e3aff413d8de269051ee33a2411d4f9424
Saikat Mitra <saikatmitra91@gmail.com>
2024-04-16 12:08:32 +0530

    fix: align tabs with other contents in sample card

--- apps/web/components/ui/tabs.tsx
+++ apps/web/components/ui/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-9 items-center justify-center rounded-lg p-1 text-muted-foreground",
+      "inline-flex h-9 items-center justify-center rounded-lg text-muted-foreground",
       className,
     )}
     {...props}

9ba63d765ebe1811c2ba6cebf1476e2c879f7325
Saikat Mitra <saikatmitra91@gmail.com>
2024-04-22 16:02:57 +0530

    test: add test for JSON mode

--- packages/ai/src/providers/google/index.test.ts
+++ packages/ai/src/providers/google/index.test.ts
@@ -18,6 +18,26 @@ describe.concurrent("test google ai provider", () => {
     expect(chatCompletion.choices[0]?.message.content).not.toContain("Asia");
   }, 10000);
 
+  test("test JSON mode fro gemini-1.5-pro-latest using passthrough parameter", async () => {
+    const chatCompletion = await GoogleAIProvider.chat({
+      model: "gemini-1.5-pro-latest",
+      messages: [
+        {
+          role: "user",
+          content: `List a few popular cookie recipes using this JSON schema: {'type': 'object', 'properties': { 'recipe_name': {'type': 'string'}}}`,
+        },
+      ],
+      response_mime_type: "application/json",
+    });
+    expect(chatCompletion.choices.length).toBeGreaterThan(0);
+    expect(
+      JSON.parse(chatCompletion.choices[0]?.message.content!),
+    ).toBeDefined();
+    expect(
+      JSON.parse(chatCompletion.choices[0]?.message.content!)[0]["recipe_name"],
+    ).toBeDefined();
+  }, 10000);
+
   test("passthrough model parameters works on gemini-1.5-pro", async () => {
     const chatCompletion = await GoogleAIProvider.chat({
       model: "gemini-1.5-pro-latest",

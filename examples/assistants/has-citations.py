2b36546751091119f0d3500073c1cff2ec0ae427
Arjun Attam <arjunattam@gmail.com>
2024-04-25 17:16:08 +0530

    citation has check

--- examples/assistants/has-citations.py
+++ examples/assistants/has-citations.py
@@ -4,10 +4,14 @@ import json
 def evaluate(output, inputs):
     metadata = output.get("metadata")
     citations = metadata.get("citations") if metadata else None
-    return [
-        {
-            "score": 1 if citations else 0,
-            "message": json.dumps(citations[0]) if citations else "No citations found",
-            "name": "has-citations",
-        }
-    ]
+    should_have_citation = inputs.get("should have citations") == "yes"
+    if should_have_citation:
+        return [
+            {
+                "score": 1 if citations else 0,
+                "message": json.dumps(citations[0]) if citations else "No citations found",
+                "name": "has-citations",
+            }
+        ]
+    else:
+        return []

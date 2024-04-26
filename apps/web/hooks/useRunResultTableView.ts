5e6d76c1e321fe359ab81d825037e5a34c56c12e
Saikat Mitra <saikatmitra91@gmail.com>
2024-04-26 10:46:44 +0530

    test: json objects to load duckdb

--- apps/web/hooks/useRunResultTableView.ts
+++ apps/web/hooks/useRunResultTableView.ts
@@ -33,11 +33,108 @@ if (typeof window !== "undefined") {
     await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
     const conn = await db.connect();
     conn.query("LOAD json");
-    const streamResponse = await fetch(
-      `https://assets-test.empirical.run/runs%2F102%2Foutput.json`,
-    );
-    const data = await streamResponse.json();
+    conn.query("INSTALL json");
+    // const streamResponse = await fetch(
+    //   `https://assets-test.empirical.run/4b61.jsonl`,
+    // );
+    // const data = await streamResponse.text();
 
+    const jsonData = [
+      {
+        config: {
+          run_config: {
+            type: "model",
+            provider: "openai",
+            model: "gpt-4-turbo-preview",
+            prompt:
+              "Extract the name, age and location from the message, and respond with a JSON object. If an entity is missing, respond with null.\n\nMessage: {{user_message}}",
+            parameters: { response_format: { type: "json_object" } },
+            scorers: [{ type: "is-json" }],
+            name: "Run #4b61: gpt-4-turbo-preview",
+          },
+          id: "4b61",
+          dataset_config: { id: "d97a7e79f2eb68c5b7d2c101292d3a00" },
+          created_at: "2024-04-26T00:50:25.986Z",
+        },
+        sample: null,
+      },
+      {
+        config: {
+          run_config: {
+            type: "model",
+            provider: "openai",
+            model: "gpt-4-turbo-preview",
+            prompt:
+              "Extract the name, age and location from the message, and respond with a JSON object. If an entity is missing, respond with null.\n\nMessage: {{user_message}}",
+            parameters: { response_format: { type: "json_object" } },
+            scorers: [{ type: "is-json" }],
+            name: "Run #4b61: gpt-4-turbo-preview",
+          },
+          id: "4b61",
+          dataset_config: { id: "d97a7e79f2eb68c5b7d2c101292d3a00" },
+          created_at: "2024-04-26T00:50:25.986Z",
+        },
+        sample: {
+          inputs: {
+            user_message:
+              "This is Alice. I am a nurse from Maryland. I was born in 1990.",
+          },
+          output: {
+            value:
+              '{\n  "name": "Alice",\n  "age": 33,\n  "location": "Maryland"\n}',
+            tokens_used: 80,
+            finish_reason: "stop",
+            latency: 1943,
+          },
+          dataset_sample_id: "2",
+          created_at: "2024-04-26T00:50:27.929Z",
+          run_id: "4b61",
+          scores: [{ score: 1, name: "is-json", message: "" }],
+        },
+      },
+      {
+        config: {
+          run_config: {
+            type: "model",
+            provider: "openai",
+            model: "gpt-4-turbo-preview",
+            prompt:
+              "Extract the name, age and location from the message, and respond with a JSON object. If an entity is missing, respond with null.\n\nMessage: {{user_message}}",
+            parameters: { response_format: { type: "json_object" } },
+            scorers: [{ type: "is-json" }],
+            name: "Run #4b61: gpt-4-turbo-preview",
+          },
+          id: "4b61",
+          dataset_config: { id: "d97a7e79f2eb68c5b7d2c101292d3a00" },
+          created_at: "2024-04-26T00:50:25.986Z",
+        },
+        sample: {
+          inputs: {
+            user_message:
+              "Hi my name is John Doe. I'm 26 years old and I work in real estate.",
+          },
+          output: {
+            value:
+              '{\n  "name": "John Doe",\n  "age": 26,\n  "location": null\n}',
+            tokens_used: 80,
+            finish_reason: "stop",
+            latency: 2405,
+          },
+          dataset_sample_id: "1",
+          created_at: "2024-04-26T00:50:28.391Z",
+          run_id: "4b61",
+          scores: [{ score: 1, name: "is-json", message: "" }],
+        },
+      },
+    ];
+
+    // const lines = data.split(/\n/);
+    // const jsonData = lines
+    //   .filter((l) => !!l)
+    //   .map((line) => {
+    //     return JSON.parse(line);
+    //   });
+    const encoder = new TextEncoder();
     // await db.registerFileBuffer(
     //   "file.json",
     //   new Uint8Array(await streamResponse.arrayBuffer()),
@@ -46,9 +143,18 @@ if (typeof window !== "undefined") {
     //   name: "file",
     // });
     // ... or column-major format
-    await db.registerFileText("columns.json", JSON.stringify(data));
+    const buffer = encoder.encode(JSON.stringify(jsonData));
+    await db.registerFileBuffer("test", buffer);
+    // await db.registerFileText("test", JSON.stringify(jsonData));
     // ... with typed insert options
-    await conn.insertJSONFromPath("columns.json", { name: "columns" });
+    await conn.insertJSONFromPath("test", {
+      name: "run",
+      schema: "main",
+    });
+    const results = await conn.query("SELECT * FROM main.run");
+    console.log(results);
+    //@ts-ignore
+    window.results = results;
     return conn;
   };
 }

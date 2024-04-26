49380f1b80408cac4ca3b4f534f74bd9f898208e
Saikat Mitra <saikatmitra91@gmail.com>
2024-04-26 06:25:51 +0530

    feat: ability to add scorers outside of runs in empiricalrc config

--- packages/cli/src/types/index.ts
+++ packages/cli/src/types/index.ts
@@ -1,7 +1,8 @@
-import { RunConfig, DatasetConfig } from "@empiricalrun/types";
+import { RunConfig, DatasetConfig, Scorer } from "@empiricalrun/types";
 
 export type RunsConfig = {
+  $schema?: string;
   runs: RunConfig[];
   dataset: DatasetConfig;
-  $schema?: string;
+  scorers?: Scorer[];
 };
